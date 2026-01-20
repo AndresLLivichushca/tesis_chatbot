64ea89c7420948808201ac34042c46a426bd7c60
Andres <andresllivichushca@gmail.com>
2026-01-20 14:54:34 -0500

    Sincronización de nombres en rutas y controlador

--- src/interfaces/http/routes/manychat.routes.ts
+++ src/interfaces/http/routes/manychat.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { manychatController } from '../controllers/manychat.controller';
+// Importamos la función exacta que exportaste en el controlador
+import { handleIncoming } from '../controllers/manychat.controller';
 import { manychatAuthMiddleware } from '../middlewares/manychatAuth.middleware';
 
 export const manychatRouter = Router();
 
-// Webhook seguro (secret header)
-manychatRouter.post('/', manychatAuthMiddleware, manychatController.handleIncoming);
+/**
+ * Webhook seguro para ManyChat
+ * Usamos la función 'handleIncoming' directamente
+ */
+manychatRouter.post('/', manychatAuthMiddleware, handleIncoming);
